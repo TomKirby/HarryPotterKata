f57bf47b24d83e31377766d98d24be260b0dc708
Tom Kirby <tkirby@smartspaceplc.com>
2021-04-02 13:22:53 +0100

    Inital Boiler Plate Commit

---
+++ HPKata.Tests/HPBookStoreTests.cs
@@ -0,0 +1,15 @@
+﻿using NuGet.Frameworks;
+using NUnit.Framework;
+
+namespace HPKata.Tests
+{
+    [TestFixture]
+    public class HPBookStoreTests
+    {
+        [Test]
+        public void ShouldRun()
+        {
+            Assert.Fail("I Failed");
+        }
+    }
+}
